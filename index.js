885f9ae7621f0d3caa47e093c39a962ad179318a
Gina Russo <gpr0496@gmail.com>
2023-06-09 22:59:33 -0400

    updated navigation

--- index.js
+++ index.js
@@ -10,3 +10,23 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
+
+// Get the button
+let arrowUp = document.getElementById('arrow-up');
+
+// When the user scrolls down 20px from the top of the document, show the button
+ window.onscroll = function () {
+    scrollFunction()
+}
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        arrowUp.style.display = 'block'
+    } else {
+        arrowUp.style.display = 'none'
+    }
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0
+    document.documentElement.scrollTop = 0
+}
